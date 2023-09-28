1cf11492f7e6daa16d75ebe7ed3ab75f8e647bd7
STUDENT Marian A. Rogojina <Marian.A.Rogojina@students.ittralee.ie>
2023-09-28 11:04:13 +0100

    Signed-off-by: STUDENT Marian A. Rogojina <Marian.A.Rogojina@students.ittralee.ie>

--- My project (1)/Assets/CubeControlScript.cs
+++ My project (1)/Assets/CubeControlScript.cs
@@ -5,19 +5,24 @@ using UnityEngine.UIElements;
 
 public class CubeControlScript : MonoBehaviour
 {
+    Rigidbody myRB;
+    
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 5);
+        myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) { transform.position += Vector3.forward * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.W)) { transform.position += transform.forward * Time.deltaTime; }
         if (Input.GetKey(KeyCode.A)) { transform.position += Vector3.left * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.S)) { transform.position += Vector3.back * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S)) { transform.position -= transform.forward * Time.deltaTime; }
         if (Input.GetKey(KeyCode.D)) { transform.position += Vector3.right * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.Space)) { myRB.AddExplosionForce(0, transform.position + Vector3.down, 0); }
+        if (Input.GetKey(KeyCode.Q)) { transform.Rotate(Vector3.left, -90 * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.E)) { transform.Rotate(Vector3.right,-90 * Time.deltaTime); }
 
 
     }
