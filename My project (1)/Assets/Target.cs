3de6a22524517b65360730169f260d4598fc9b8f
STUDENT Marian A. Rogojina <Marian.A.Rogojina@students.ittralee.ie>
2024-11-14 10:07:23 +0000

    Added character animation and a few land props
    Signed-off-by: STUDENT Marian A. Rogojina <Marian.A.Rogojina@students.ittralee.ie>

--- My project (1)/Assets/Target.cs
+++ My project (1)/Assets/Target.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Target : MonoBehaviour
@@ -7,14 +8,19 @@ public class Target : MonoBehaviour
     [Header("Stats")]
     public int health;
 
+    public bool IsDead { get; private set; }
+    
+
     public void TakeDamage(int damage)
     {
+
         health -= damage;
 
         if (health <= 0)
             DestroyEnemy();
     }
 
+    
     public void DestroyEnemy()
     {
         Destroy(gameObject);
